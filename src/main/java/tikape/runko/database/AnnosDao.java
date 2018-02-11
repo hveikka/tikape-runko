efc7ba9a8ed41e5a983760822c610c39b7cba69f
anitapenttila <anitapenttila@anitas-mbp.home>
2018-02-11 10:31:03 +0200

    AnnosDao ja RaakaAineDao

--- src/main/java/tikape/runko/database/AnnosDao.java
+++ src/main/java/tikape/runko/database/AnnosDao.java
@@ -5,9 +5,14 @@
  */
 package tikape.runko.database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import tikape.runko.domain.Annos;
+
 
 /**
  *
@@ -23,17 +28,120 @@ public class AnnosDao implements Dao<Annos, Integer> {
 
     @Override
     public Annos findOne(Integer key) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Annos WHERE id = ?");
+        stmt.setObject(1, key);
+
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+
+        Integer id = rs.getInt("id");
+        String nimi = rs.getString("nimi");
+
+        Annos a = new Annos(id, nimi);
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return a;
     }
 
     @Override
     public List<Annos> findAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM Annos");
+
+        ResultSet rs = stmt.executeQuery();
+        List<Annos> annokset = new ArrayList<>();
+        while (rs.next()) {
+            Integer id = rs.getInt("id");
+            String nimi = rs.getString("nimi");
+
+            annokset.add(new Annos(id, nimi));
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return annokset;
     }
 
     @Override
     public void delete(Integer key) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection conn = database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM Annos WHERE id = " + key);
+        stmt.executeUpdate();
+        stmt.close();
+        conn.close();
+    }
+    public Annos saveOrUpdate(Annos object) throws SQLException {
+
+        if (object.getId()== null) {
+            return save(object);
+        } else {
+            return update(object);
+        }
+    }
+    private Annos save(Annos annos) throws SQLException {
+        
+        Connection conn = database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM Annos"
+                + " WHERE nimi = ?");
+        stmt.setString(1, annos.getNimi());
+
+        ResultSet rs = stmt.executeQuery();
+        if (rs.next()) {
+            stmt.close();
+            return null;
+        }
+        stmt.close();
+
+        stmt = conn.prepareStatement("INSERT INTO Annos"
+                + " (nimi)"
+                + " VALUES (?)");
+        stmt.setString(1, annos.getNimi());
+
+
+        stmt.executeUpdate();
+        stmt.close();
+
+        stmt = conn.prepareStatement("SELECT * FROM Annos"
+                + " WHERE nimi = ?");
+        stmt.setString(1, annos.getNimi());
+
+        rs = stmt.executeQuery();
+        rs.next(); 
+
+        Annos a = new Annos(rs.getInt("id"), rs.getString("nimi"));
+
+        stmt.close();
+        rs.close();
+
+        conn.close();
+
+        return a;
+    }
+
+    private Annos update(Annos annos) throws SQLException {
+
+        Connection conn = database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("UPDATE Annos SET"
+                + " nimi = ? WHERE id = ?");
+        stmt.setString(1, annos.getNimi());
+        stmt.setInt(2, annos.getId());
+
+
+        stmt.executeUpdate();
+
+        stmt.close();
+        conn.close();
+
+        return annos;
     }
     
 }
--- src/main/java/tikape/runko/database/RaakaAineDao.java
+++ src/main/java/tikape/runko/database/RaakaAineDao.java
@@ -5,7 +5,11 @@
  */
 package tikape.runko.database;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import tikape.runko.domain.RaakaAine;
 
@@ -23,17 +27,121 @@ public class RaakaAineDao implements Dao<RaakaAine, Integer> {
 
     @Override
     public RaakaAine findOne(Integer key) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM RaakaAine WHERE id = ?");
+        stmt.setObject(1, key);
+
+        ResultSet rs = stmt.executeQuery();
+        boolean hasOne = rs.next();
+        if (!hasOne) {
+            return null;
+        }
+
+        Integer id = rs.getInt("id");
+        String nimi = rs.getString("nimi");
+
+        RaakaAine raakaAine = new RaakaAine(id, nimi);
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return raakaAine;
     }
 
     @Override
     public List<RaakaAine> findAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection connection = database.getConnection();
+        PreparedStatement stmt = connection.prepareStatement("SELECT * FROM RaakaAine");
+
+        ResultSet rs = stmt.executeQuery();
+        List<RaakaAine> raakaAineet = new ArrayList<>();
+        while (rs.next()) {
+            
+            Integer id = rs.getInt("id");
+            String nimi = rs.getString("nimi");
+
+            raakaAineet.add(new RaakaAine(id, nimi));
+        }
+
+        rs.close();
+        stmt.close();
+        connection.close();
+
+        return raakaAineet;
     }
 
     @Override
     public void delete(Integer key) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection conn = database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM RaakaAine WHERE id = " + key);
+        stmt.executeUpdate();
+        stmt.close();
+        conn.close();
+    }
+    public RaakaAine saveOrUpdate(RaakaAine object) throws SQLException {
+
+        if (object.getId()== null) {
+            return save(object);
+        } else {
+            return update(object);
+        }
+    }
+    private RaakaAine save(RaakaAine raakaAine) throws SQLException {
+        
+        Connection conn = database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM RaakaAine"
+                + " WHERE nimi = ?");
+        stmt.setString(1, raakaAine.getNimi());
+
+        ResultSet rs = stmt.executeQuery();
+        if (rs.next()) {
+            stmt.close();
+            return null;
+        }
+        stmt.close();
+        
+        stmt = conn.prepareStatement("INSERT INTO RaakaAine"
+                + " (nimi)"
+                + " VALUES (?)");
+        stmt.setString(1, raakaAine.getNimi());
+
+
+        stmt.executeUpdate();
+        stmt.close();
+
+        stmt = conn.prepareStatement("SELECT * FROM RaakaAine"
+                + " WHERE nimi = ?");
+        stmt.setString(1, raakaAine.getNimi());
+
+        rs = stmt.executeQuery();
+        rs.next(); 
+
+        RaakaAine ra = new RaakaAine(rs.getInt("id"), rs.getString("nimi"));
+
+        stmt.close();
+        rs.close();
+
+        conn.close();
+
+        return ra;
+    }
+
+    private RaakaAine update(RaakaAine raakaAine) throws SQLException {
+
+        Connection conn = database.getConnection();
+        PreparedStatement stmt = conn.prepareStatement("UPDATE RaakaAine SET"
+                + " nimi = ? WHERE id = ?");
+        stmt.setString(1, raakaAine.getNimi());
+        stmt.setInt(2, raakaAine.getId());
+
+
+        stmt.executeUpdate();
+
+        stmt.close();
+        conn.close();
+
+        return raakaAine;
     }
     
 }
