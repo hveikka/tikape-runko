f73fc5679387d270557d77740799ad3bb57c34c1
Kasperi Kuuskoski <kasperi.kuuskoski@gmail.com>
2018-02-22 11:19:05 +0200

    porttiasetukset

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -15,6 +15,10 @@ import tikape.runko.domain.RaakaAine;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        if (System.getenv("PORT") != null) {
+            Spark.port(Integer.valueOf(System.getenv("PORT")));
+        }
+        
         Database database = new Database("jdbc:sqlite:annokset.db");
         database.init();
         
