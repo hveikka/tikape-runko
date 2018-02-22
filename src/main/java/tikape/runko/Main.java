2bc46708de6d7353eb3f500e7fb93d29b136906a
Kasperi Kuuskoski <kasperi.kuuskoski@gmail.com>
2018-02-22 11:57:41 +0200

    testii

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -15,9 +15,9 @@ import tikape.runko.domain.RaakaAine;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        if (System.getenv("PORT") != null) {
-            Spark.port(Integer.valueOf(System.getenv("PORT")));
-        }
+//        if (System.getenv("PORT") != null) {
+//            Spark.port(Integer.valueOf(System.getenv("PORT")));
+//        }
         
         Database database = new Database("jdbc:sqlite:annokset.db");
         database.init();
--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -13,10 +13,10 @@ public class Database {
     }
 
     public Connection getConnection() throws SQLException {
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        if (dbUrl != null && dbUrl.length() > 0) {
-            return DriverManager.getConnection(dbUrl);
-        }
+//        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+//        if (dbUrl != null && dbUrl.length() > 0) {
+//            return DriverManager.getConnection(dbUrl);
+//        }
         return DriverManager.getConnection(databaseAddress);
     }
 
